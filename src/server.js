a366f7a1eedfd3f7675b1224b8deffadc6a6b401
CuTrung <cuphanthanhtrung4112003@gmail.com>
2022-09-06 09:30:30 +0700

    update_06_09_22

--- src/server.js
+++ src/server.js
@@ -1,9 +1,9 @@
 import express from 'express';
 import conFigViewEngine from './configs/viewEngine';
-
+require('dotenv').config();
 
 const app = express();
-const port = 3000;
+const port = process.env.PORT;
 
 conFigViewEngine(app);
 
