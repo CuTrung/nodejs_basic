57a7195b2d023f20a5227e94c8aaaf4bf552ce51
CuTrung <cuphanthanhtrung4112003@gmail.com>
2022-09-09 08:04:02 +0700

    update 09_09_22

--- src/server.js
+++ src/server.js
@@ -5,11 +5,15 @@ import initAPIRoute from './route/api';
 // import connection from './configs/connectDB';
 
 require('dotenv').config();
+const morgan = require('morgan');
 
 const app = express();
 const port = process.env.PORT || 8081;
 
-// Cấu hình express gửi thông tin lên server
+app.use(morgan('combined'));
+
+
+// Cấu hình express parse data phức tạp từ request thành json
 app.use(express.urlencoded({ extends: true }));
 app.use(express.json());
 
@@ -21,6 +25,12 @@ initWebRoute(app);
 
 // init api route
 initAPIRoute(app);
+
+// handle 404 not found
+app.use((req, res) => {
+    return res.render('404.ejs');
+})
+
 
 app.listen(port, () => {
     console.log("Run project");
